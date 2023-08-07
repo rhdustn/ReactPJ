5806e37835b1c383ab8f26c0f55cfdcc93250dbf
zerohoney <zerohoney97@gmail.com>
2023-08-07 17:54:41 +0900

    addMediaQuery

--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,27 @@
-import './App.css';
+import "./App.css";
 
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
-import { Main, Plan,Mypage,EditProflie } from './pages';
+import { Main, Plan, Mypage, EditProflie } from "./pages";
+import { useMediaQuery } from "react-responsive";
 
 function App() {
+  const isMobile = useMediaQuery({
+    query: "(max-width:768px)",
+  });
   return (
     <div className="App">
       <Routes>
-        {/* <Route path='/' element={<Main />} />
-        <Route path='/plan' element={<Plan />} /> */}
-        <Route path='/mypage' element={<Mypage />} />
-        <Route path='/editproflie' element={<EditProflie />} />
+        <Route path='/' element={isMobile ? <>모바일이다!</> :<Main />} />
+        <Route path='/plan' element={isMobile ? <>모바일이다!</> :<Plan />} />
+        <Route
+          path="/mypage"
+          element={isMobile ? <>모바일이다!</> : <Mypage />}
+        />
+        <Route
+          path="/editproflie"
+          element={isMobile ? <>와! 모바일!</> : <EditProflie />}
+        />
       </Routes>
     </div>
   );
