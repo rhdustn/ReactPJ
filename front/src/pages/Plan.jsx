571e4bc156e5a08896cbf9437dd9ec91ce45815c
9oodam <godamcity98@gmail.com>
2023-08-07 17:30:17 +0900

    plan page 바꾸는 중

--- front/src/pages/Plan.jsx
+++ front/src/pages/Plan.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BottomNav from '../components/nav/BottomNav'
 import PlanTop from '../components/plan/PlanTop'
 import TopNav from '../components/nav/TopNav'
@@ -6,6 +6,13 @@ import PlanMid from '../components/plan/PlanMid'
 import PlanBottom from '../components/plan/PlanBottom'
 
 const Plan = () => {
+  const [dayNum, setDayNum] = useState();
+
+  // 지도 받아오기
+  const getMap = () => {
+
+  }
+
   return (
     <>
       <TopNav />
