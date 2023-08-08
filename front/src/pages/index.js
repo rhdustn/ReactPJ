7f0606e2190d332e1710e9ef37c14116d7dffebc
9oodam <godamcity98@gmail.com>
2023-08-08 10:56:34 +0900

    PlanAll 페이지 삭제 -> 마이페이지로 이동 예정

--- front/src/pages/index.js
+++ front/src/pages/index.js
@@ -1,5 +1,4 @@
 import Main from './Main'
-import PlanAll from './PlanAll'
 import Plan from './Plan'
 import AddPlace from './AddPlace'
 import EditPlace from './EditPlace'
@@ -7,4 +6,4 @@ import EditPlace from './EditPlace'
 import Mypage from './Mypage'
 import EditProflie from './EditProflie'
 
-export {Main, PlanAll, Plan, AddPlace, EditPlace, Mypage,EditProflie}
+export {Main, Plan, AddPlace, EditPlace, Mypage,EditProflie}
