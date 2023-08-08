93affcd4c01f4e9315434f5fc0d8573c8c09b05a
biggsky <sunghyun0624@naver.com>
2023-08-08 09:47:27 +0900

    commit

--- back/controllers/mainControllers.js
+++ back/controllers/mainControllers.js
@@ -1,2 +1,9 @@
 const {User, Board} = require("../models");
 
+exports.testcon = async (req,res)=>{
+    try {
+        console.log("1");
+    } catch (error) {
+        console.log(error);
+    }
+}
