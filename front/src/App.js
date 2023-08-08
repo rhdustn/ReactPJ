5564deb6a1b4c1050c9f35fa38b17274e3171657
zerohoney <zerohoney97@gmail.com>
2023-08-08 11:03:05 +0900

    addMobileBody

--- front/src/App.js
+++ front/src/App.js
@@ -5,20 +5,66 @@ import { Routes, Route } from "react-router-dom";
 import { Main, Plan, Mypage, EditProflie } from "./pages";
 import { EditProfliePc, MainPc, MypagePc, PlanPc } from "./pagesPc";
 import { useMediaQuery } from "react-responsive";
-
+import { styled } from "styled-components";
 function App() {
   const isMobile = useMediaQuery({
     query: "(max-width:768px)",
   });
+
+  const MobileBody = styled.div`
+    width: 80%;
+    margin: auto;
+  `;
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={isMobile ? <Main /> : <MainPc />} />
-        <Route path="/plan" element={isMobile ? <Plan /> : <PlanPc />} />
-        <Route path="/mypage" element={isMobile ? <Mypage /> : <MypagePc />} />
+        <Route
+          path="/"
+          element={
+            isMobile ? (
+              <Main />
+            ) : (
+              <MobileBody>
+                <MainPc />
+              </MobileBody>
+            )
+          }
+        />
+        <Route
+          path="/plan"
+          element={
+            isMobile ? (
+              <Plan />
+            ) : (
+              <MobileBody>
+                <PlanPc />
+              </MobileBody>
+            )
+          }
+        />
+        <Route
+          path="/mypage"
+          element={
+            isMobile ? (
+              <Mypage />
+            ) : (
+              <MobileBody>
+                <MypagePc />
+              </MobileBody>
+            )
+          }
+        />
         <Route
           path="/editproflie"
-          element={isMobile ? <EditProflie /> : <EditProfliePc />}
+          element={
+            isMobile ? (
+              <EditProflie />
+            ) : (
+              <MobileBody>
+                <EditProfliePc />
+              </MobileBody>
+            )
+          }
         />
       </Routes>
     </div>
