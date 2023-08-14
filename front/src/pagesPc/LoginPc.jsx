e81a22be3a0dc193f7acfa63dcfee448f3d16717
zerohoney <zerohoney97@gmail.com>
2023-08-14 13:15:41 +0900

    웹크롤링테스트

--- front/src/pagesPc/LoginPc.jsx
+++ front/src/pagesPc/LoginPc.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React from "react";
 
-import SignupTop from '../componentsPc/signup/SignupTopPc'
-import SignupMid from '../componentsPc/signup/SignupMidPc'
-import SignupBottom from '../componentsPc/signup/SignupBottomPc'
+import SignupTop from "../componentsPc/signup/SignupTopPc";
+import SignupMid from "../componentsPc/signup/SignupMidPc";
+import SignupBottom from "../componentsPc/signup/SignupBottomPc";
+import { SignUpContainer } from "../componentsPc/signup/SignupPc.styled";
 
 const LoginPc = () => {
   return (
     <>
-      <SignupTop />
-      <SignupMid page={'로그인'} />
-      <SignupBottom page={'로그인'} />
+      <SignUpContainer>
+        <SignupTop />
+        <SignupMid page={"로그인"} />
+        <SignupBottom page={"로그인"} />
+      </SignUpContainer>
     </>
-  )
-}
+  );
+};
 
-export default LoginPc
+export default LoginPc;
