9128240ae8f801e856aec4c6f5e9fa103f66d62e
9oodam <godamcity98@gmail.com>
2023-08-17 15:28:33 +0900

    사진 띄우기

--- front/src/components/plan/PlanBottom.js
+++ front/src/components/plan/PlanBottom.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useQueries } from 'react-query'
-import axios from 'axios'
 
 import {
   PlanBottomBox,
@@ -15,7 +13,7 @@ import {
   EditPlanBtn,
 } from "./Plan.styled";
 
-import city from "../../img/places/city.jpeg";
+import noImage from "../../img/icons/no-image.png";
 import { useNavigate } from "react-router-dom";
 import { useQueries } from "react-query";
 import axios from "axios";
@@ -64,7 +62,20 @@ const PlanBottom = ({ isScrolled, gptAnswerSaved, userChoiceSaved }) => {
       };
     })
   );
+  
+  useEffect(() => {
+    //  attractions state를 jsx에 쓰기 위하여 만든 임시 배열AttArrForMap에 0을 저장하는 로직
+    // 이미지를 저장하는 로직
+    let temp = [];
+    attractions.forEach((value, index) => {
+      temp.push(0);
+    });
+    setAttArrForMap(temp);
+  }, [attractions]);
+  
   // 여기까지 이미지를 api로 받아오는 로직, attractionsWithImg에 기존 attraction값에 img가 추가로 들어감.
+
+  
   let sd = new Date(startDate);
   let ed = new Date(endDate);
 
@@ -95,9 +106,10 @@ const PlanBottom = ({ isScrolled, gptAnswerSaved, userChoiceSaved }) => {
     return (
         <>
         <PlanBottomBox id='bottom-box'>
-            {periodArr.map((value, index) => {
-                return <PerDay key={index} period={value} index={index+1} place={planPerDay[index+1].plan} />
-            })}
+          {periodArr.map((value, index) => {
+            return <PerDay key={index} period={periodArr[index]} index={index+1} place={planPerDay[index+1].plan} attractionsWithImg={attractionsWithImg} />
+          })}
+
             <BtnBox>
                 <SavePlanBtn>저장</SavePlanBtn>
             </BtnBox>
@@ -107,7 +119,7 @@ const PlanBottom = ({ isScrolled, gptAnswerSaved, userChoiceSaved }) => {
 }
 
 // 1일마다 관광지 보여주는 부분
-const PerDay = ({ period, index, place, imgSrc }) => {
+const PerDay = ({ period, index, place, attractionsWithImg }) => {
   const nav = useNavigate();
 
   // 관광지 검색 & 추가 페이지로 이동
@@ -115,6 +127,7 @@ const PerDay = ({ period, index, place, imgSrc }) => {
     // 해당 plan의 해당 날짜에 대한 id
     nav(`/addPlace/${id}?day=${id}`);
   };
+
 
   return (
     <>
@@ -130,6 +143,12 @@ const PerDay = ({ period, index, place, imgSrc }) => {
         {/* 관광지 하나 */}
         <PerDayAttraction>
           {place.map((value, index) => {
+            let imgSrc;
+            const foundAttraction = attractionsWithImg.find(value2 => value2.name === value.name);
+            if (foundAttraction) {
+              imgSrc = foundAttraction?.img?.hits?.[0]?.largeImageURL;
+            }
+
             return (
               <RouteBox>
                 <RouteNumber>
@@ -137,9 +156,15 @@ const PerDay = ({ period, index, place, imgSrc }) => {
                   <div>{index + 1}</div>
                 </RouteNumber>
                 <RoutePlace>
-                  <div>
+                  <div className="place-box">
                     <p>{value.name}</p>
-                    <img src={city}></img>
+                    <div className="img-box">
+                      {imgSrc !== undefined ? (
+                        <img src={imgSrc}></img>
+                      ) : (
+                        <img src={noImage}></img>
+                      )}
+                    </div>
                   </div>
                 </RoutePlace>
               </RouteBox>
