e8240528373d487f8ec2d80d582ba78ffbc5f410
9oodam <godamcity98@gmail.com>
2023-08-26 16:53:36 +0900

    board pc 수정 중

--- front/src/pagesPc/BoardPc.jsx
+++ front/src/pagesPc/BoardPc.jsx
@@ -5,44 +5,40 @@ import BottomNavPc from "../componentsPc/nav/BottomNavPc";
 import { useNavigate, Link } from 'react-router-dom';
 
 
-const BoardMain =styled.div`
-width: 100%;
-height: 800px;
-/* background-color: beige; */
+const BoardMain = styled.div`
+    width: 100%;
+    height: calc(100vh - 60px);
+    /* background-color: beige; */
 
-& button{
-    box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
-    border: 0px;
-    padding: 10px;
-}
-& button:hover{
-    background-color: aliceblue;
-}
+    & .moeToCreate {
+        position: absolute;
+        right: 10%;
+        top: 5%;
+        color: #277bc0;
+        cursor: pointer;
+        box-sizing: border-box;
+        font-weight: 600;
+    }
 
-`
+    & .moeToCreate:hover {
+        transform: scale(105%);
+    }
+`;
 
 
 const BoardPc = () => {
-    const nav = useNavigate();
     return (
-        <div>
-            
+        <>
             <BoardMain>
                 <BoardTitlePc />
-                <Link to={"/boardCreate"}><button>글 작성하기</button></Link>
+                <Link to={"/boardCreate"}><div className='moeToCreate'>글 작성하기</div></Link>
 
-                <BoardSubTitlePc />
-                <BoardListPc/>
-
-                
+                <BoardListPc/>     
             </BoardMain>
 
-            
-            
             <BottomNavPc /> 
             {/* 추가함 */}
-
-        </div>
+        </>
     )
 }
 
