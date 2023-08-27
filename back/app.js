0ffc7d101163950390135348912e07310c93ffd5
zerohoney <zerohoney97@gmail.com>
2023-08-27 21:59:34 +0900

    주석

--- back/app.js
+++ back/app.js
@@ -32,6 +32,25 @@ const boardRouter = require("./routers/boardlistRouter");
 
 // const upload = multer({ storage: storage });
 
+// nginix 설정
+// location / {
+//   # First attempt to serve request as file, then
+//   # as directory, then fall back to displaying a 404.
+// root /home/front/build;
+//   try_files $uri /index.html;
+
+// }
+
+// location /api/ {
+// proxy_set_header HOST $host;
+// proxy_pass http://127.0.0.1:8080/;
+// proxy_redirect off;
+// proxy_http_version 1.1;
+//   proxy_set_header Upgrade $http_upgrade;
+//   proxy_set_header Connection 'upgrade';
+//   proxy_set_header Host $host;
+//   proxy_cache_bypass $http_upgrade;
+// }
 // 아마 form 데이터
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
